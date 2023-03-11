54285ab0a9e309debc946e558e0482468444305a
keshav <talrejakeshav.399@gmail.com>
2023-03-11 14:34:32 +0530

    resolved browser router basename in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <AppContext>
-      <BrowserRouter>
+      <BrowserRouter basename={window.location.pathname || ''}>
         <div className="flex flex-col bg-black">
           <Header />
           <Routes>
